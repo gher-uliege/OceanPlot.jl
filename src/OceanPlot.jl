8018519d3a2aa62a5780980b5f3ee27017c7a38d
Alexander Barth <barth.alexander@gmail.com>
2021-12-06 13:52:22 +0100

    add animation

--- src/OceanPlot.jl
+++ src/OceanPlot.jl
@@ -8,6 +8,8 @@ using MAT
 using NCDatasets
 using DIVAnd
 using LinearAlgebra
+using Printf
+using FFMPEG
 
 # allow for plotting with missing values
 function PyObject(a::Array{Union{T,Missing},N}) where {T,N}
@@ -39,7 +41,7 @@ function romsview(fname::AbstractString,varname,slide; kwargs...)
 
         pcolormesh(lon .- Δlon/2,lat .- Δlat/2,v; cmap="jet", kwargs...)
         set_aspect_ratio()
-        
+
         return lon,lat,v
     end
 end
@@ -211,4 +213,47 @@ function listfiles(topdir = "."; extension = "")
     return list
 end
 
+
+"""
+varname = "temp"; cl = (6.216519f0, 16.935743f0); outname = "SST.mp4"
+"""
+function animation(fname,varname,cl,outname; figprefix = replace(outname,".mp4" => "-"), framerate  = 5, qtile = (0.05,0.95))
+
+    get(v,n) = (ndims(v) == 4 ? v[:,:,end,n] : v[:,:,n] )
+
+    Dataset(fname) do ds
+        figure(figsize=(10,6));
+
+        v = ds[varname]
+
+        if cl == nothing
+            q = [quantile(skipmissing(vec(get(v,k))), qtile) for k = 1:size(v)[end]];
+            cl = (minimum(getindex.(q,1)), maximum(getindex.(q,2)))
+            @show cl
+        end
+
+        lon = NCDatasets.coord(v,"longitude")[:]
+        lat = NCDatasets.coord(v,"latitude")[:]
+
+        for n = 1:size(v)[end]
+            clf()
+            v_slice = get(v,n)
+            pcolormesh(lon,lat,nomissing(v_slice,NaN),cmap="jet");
+            OceanPlot.set_aspect_ratio();
+            clim(cl)
+            colorbar()
+            title("SST $(ds["ocean_time"][n])")
+            figname = figprefix * @sprintf("%05d.png",n)
+            @info "saving $figname"
+            savefig(figname)
+        end
+    end
+
+    FFMPEG.exe("-y",
+               "-r",string(framerate),
+               "-i",figprefix * "%*.png",
+               "-qmax","20",
+               "-b:v","50k",
+               outname)
+end
 end # module
